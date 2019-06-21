f2dd910d4f5cf50753bacf8ec856688d78771981
zhangjingwei <907132887@qq.com>
2019-06-21 14:12:38 +0200

    Final version + apache config file

--- testserver/wsgi.py
+++ testserver/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+from os.path import join,dirname,abspath
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testserver.settings')
-
+PROJECT_DIR = dirname(dirname(abspath(__file__)))
+sys.path.insert(0, PROJECT_DIR)
+os.environ["DJANGO_SETTINGS_MODULE"] = "testserver.settings"
 application = get_wsgi_application()
